33909194823d85f1ef0fda15cbf8ee7868401fa0
Christopher Rodriguez Mendoza <crodriguezm2001@alumno.ipn.mx>
2025-07-01 21:44:27 -0600

    Update init.sql

--- init.sql
+++ init.sql
@@ -1,28 +1,30 @@
-create database alumnos;
-use alumnos;
-create table alumno (
-    boleta varchar(10) PRIMARY KEY,
-    nombre varchar(64),
+CREATE DATABASE alumnos;
+USE alumnos;
+
+CREATE TABLE administrador (
+    username VARCHAR(16),
+    correo VARCHAR(32) UNIQUE,
+    password VARCHAR(32)
+);
+
+CREATE TABLE examen (
+    fecha DATE,
+    lab INT AUTO_INCREMENT PRIMARY KEY,
+    hora TINYINT
+);
+
+CREATE TABLE alumno (
+    boleta VARCHAR(10) PRIMARY KEY,
+    nombre VARCHAR(64),
     telefono BIGINT,
     curp VARCHAR(18),
     entidad VARCHAR(32),
     genero BOOLEAN,
-    fecha date,
+    fecha DATE,
     escuela VARCHAR(32),
     promedio FLOAT,
     correo VARCHAR(32),
     password VARCHAR(32),
-    data_examen int,
-
-    Foreign Key (data_examen) REFERENCES examen (lab)   
-);
-create table administrador (
-    username varchar(16),
-    correo UNIQUE VARCHAR(32),
-    password VARCHAR(32)
-);
-create table examen (
-    fecha date,
-    lab SERIAL PRIMARY KEY,
-    hora TINYINT
+    data_examen INT,
+    FOREIGN KEY (data_examen) REFERENCES examen (lab)
 );
