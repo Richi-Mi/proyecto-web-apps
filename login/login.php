7e4042c641a66ea90dae444303802dc2e1231b32
Mendoza Castañeda José Ricardo <joseanakin@outlook.es>
2025-07-01 21:21:42 -0600

    changes added

--- login/login.php
+++ login/login.php
@@ -20,6 +20,12 @@
         $response['message'] = 'Revise las credenciales ingresadas';    
     }
     else {
+        $data_exam = $row[11];
+        $query2 = "SELECT * FROM examen where lab = $data_exam";
+        
+        $result2 = pg_query($conn, $query2);
+        $row2 = pg_fetch_row($result2);
+
         $response['success'] = true;
         $response['message'] = 'Login realizado correctamente';
         $response['alumno'] = [
@@ -32,7 +38,10 @@
             'fecha' => $row[6],
             'escuela' => $row[7],
             'promedio' => $row[8],
-            'correo' => $row[9]
+            'correo' => $row[9],
+            'fecha_exam' => $row2[0],
+            'hora_exam' => $row2[1],
+            'lab_exam' => $row2[2] 
         ];
     }
     echo json_encode($response);
--- user/user.js
+++ user/user.js
@@ -1,8 +1,11 @@
 const cont = document.getElementById('info-alumno');
 const data = JSON.parse(localStorage.getItem('data'))
 cont.innerHTML = `
-    <p><strong>Nombre:</strong> ${data.nombre}</p>
-    <p><strong>Boleta:</strong> ${data.boleta}</p>
+    <p><strong> Nombre:</strong> ${data.nombre}</p>
+    <p><strong> Boleta:</strong> ${data.boleta}</p>
+    <p><strong> Fecha de examen:</strong> ${data.fecha_exam}</p>
+    <p><strong> Laboratorio: </strong> ${data.hora_exam}</p>
+    <p><strong> Hora de examen: :</strong> ${data.lab_exam}</p>
 `;
 document.querySelector('.btn-primary').addEventListener('click', () => {
     window.open(`generarPDF.php?boleta=${data.boleta}`)
