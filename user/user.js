05fa311b85f52175c357b39edd7cd1f84807d40c
Mendoza Castañeda José Ricardo <joseanakin@outlook.es>
2025-07-01 20:14:15 -0600

    OMIGI ya genera el PDF

--- user/user.js
+++ user/user.js
@@ -1,8 +1,9 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const cont = document.getElementById('info-alumno');
-    const data = JSON.parse(localStorage.getItem('data'))
-    cont.innerHTML = `
-        <p><strong>Nombre:</strong> ${data.nombre}</p>
-        <p><strong>Boleta:</strong> ${data.boleta}</p>
-    `;
-});
+const cont = document.getElementById('info-alumno');
+const data = JSON.parse(localStorage.getItem('data'))
+cont.innerHTML = `
+    <p><strong>Nombre:</strong> ${data.nombre}</p>
+    <p><strong>Boleta:</strong> ${data.boleta}</p>
+`;
+document.querySelector('.btn-primary').addEventListener('click', () => {
+    window.open(`generarPDF.php?boleta=${data.boleta}`)
+})
